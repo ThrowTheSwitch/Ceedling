9398c83dfb65f6e6f998fe7dc3e43e8f8f045fa8
mkarlesky <mkarlesky@f01913a8-9348-4cf6-9845-091d89bf350d>
2010-01-26 02:01:21 +0000

    moved rake rule & task tests from unit tests to integration tests as they involve use of rake and file off disk; modified project rake file to recognize new class of test
    git-svn-id: https://ceedling.svn.sourceforge.net/svnroot/ceedling/trunk@14 f01913a8-9348-4cf6-9845-091d89bf350d

--- rakefile.rb
+++ rakefile.rb
@@ -4,6 +4,9 @@ require 'rake/clean'
 require 'rake/testtask'
 require 'rakefile_helper'
 
+TEST_FILE_SUFFIX  = '_test.rb'
+TEST_FILE_PATTERN = "*#{TEST_FILE_SUFFIX}"
+
 include RakefileHelpers
 
 
@@ -12,16 +15,18 @@ task :default => ['test:all']
 task :cruise => [:default]
 
 
-unit_test_pattern   = 'test/unit/*_test.rb'
-system_test_pattern = 'test/system/*_test.rb'
+unit_test_pattern           = "test/unit/#{TEST_FILE_PATTERN}"
+integrations_test_pattern   = "test/integration/#{TEST_FILE_PATTERN}"
+system_test_pattern         = "test/system/#{TEST_FILE_PATTERN}"
 
-ALL_UNIT_TESTS   = FileList[unit_test_pattern]
-ALL_SYSTEM_TESTS = FileList[system_test_pattern]
+ALL_UNIT_TESTS          = FileList[unit_test_pattern]
+ALL_INTEGRATION_TESTS   = FileList[integrations_test_pattern]
+ALL_SYSTEM_TESTS        = FileList[system_test_pattern]
 
 
 namespace :test do
-  desc "Run all unit and system tests"
-  task :all => ['test:unit:all', 'test:system:all']
+  desc "Run all unit, integration, and system tests"
+  task :all => ['test:unit:all', 'test:integration:all', 'test:system:all']
 
   Rake::TestTask.new('unit:all') do |t|
     t.pattern = unit_test_pattern
@@ -29,13 +34,16 @@ namespace :test do
   end
 
   namespace :unit do
-    ALL_UNIT_TESTS.each do |test|
-      base_file = File.basename(test).gsub(/_test\.rb/, '')
-      desc base_file
-      Rake::TestTask.new(base_file) do |t|
-        t.test_files = [test]
-      end
-    end
+    create_test_tasks(ALL_UNIT_TESTS)
+  end
+
+  Rake::TestTask.new('integration:all') do |t|
+    t.pattern = integrations_test_pattern
+    t.verbose = true
+  end
+
+  namespace :integration do
+    create_test_tasks(ALL_INTEGRATION_TESTS)
   end
 
   Rake::TestTask.new('system:all') do |t|
@@ -44,13 +52,7 @@ namespace :test do
   end
 
   namespace :system do
-    ALL_SYSTEM_TESTS.each do |test|
-      base_file = File.basename(test).gsub(/_test\.rb/, '')
-      desc base_file
-      Rake::TestTask.new(base_file) do |t|
-        t.test_files = [test]
-      end
-    end
+    create_test_tasks(ALL_SYSTEM_TESTS)
   end
 
 end
--- rakefile_helper.rb
+++ rakefile_helper.rb
@@ -8,6 +8,16 @@ module RakefileHelpers
     $stdout.flush
     $stderr.flush
   end
+
+  def create_test_tasks(test_list)
+    test_list.each do |test|
+      base_file = File.basename(test).gsub(/#{Regexp.escape(TEST_FILE_SUFFIX)}/, '')
+      desc base_file
+      Rake::TestTask.new(base_file) do |t|
+        t.test_files = [test]
+      end
+    end
+  end
   
 end
 
