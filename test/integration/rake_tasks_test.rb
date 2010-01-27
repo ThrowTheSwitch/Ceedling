76697e7819a033926b1a891b6aaf5fe942c091a1
mkarlesky <mkarlesky@f01913a8-9348-4cf6-9845-091d89bf350d>
2010-01-27 02:01:57 +0000

    fleshed out rake tasks integration tests
    git-svn-id: https://ceedling.svn.sourceforge.net/svnroot/ceedling/trunk@16 f01913a8-9348-4cf6-9845-091d89bf350d

--- test/integration/rake_tasks_test.rb
+++ test/integration/rake_tasks_test.rb
@@ -12,9 +12,26 @@ class RakeTasksTest < Test::Unit::TestCase
   def teardown
     Rake.application = nil
   end
+
+
+  should "set verbosity level 0" do
+    # set up expectations
+    @configurator.expects.set_verbosity(0)
+    
+    # invoke the task
+    @rake['verbosity'].invoke('0')
+  end
+
+  should "set verbosity level 2" do
+    # set up expectations
+    @configurator.expects.set_verbosity(2)
+    
+    # invoke the task
+    @rake['verbosity'].invoke(2)
+  end
   
   
-  should "clean all test results and invoke all tests" do
+  should "invoke all tests" do
     all_tests = ['tests/test_yo.c', 'tests/test.mama.c']
 
     redefine_global_constant('COLLECTION_ALL_TESTS', all_tests)
@@ -26,6 +43,59 @@ class RakeTasksTest < Test::Unit::TestCase
     # invoke the task
     @rake['tests:all'].invoke
   end
-  
+
+  should "invoke a test by its test file name" do
+    all_tests = ['tests/test_yo.c', 'tests/test.mama.c']
+
+    redefine_global_constant('COLLECTION_ALL_TESTS', all_tests)
+    redefine_global_constant('PROJECT_TEST_FILE_PREFIX', 'test_')
+    setup()
+
+    # set up expectations
+    @test_invoker.expects.invoke_tests('tests/test_yo.c')
+    
+    # invoke the task
+    @rake['tests:test_yo.c'].invoke
+  end
+
+  should "invoke a test by its source file name" do
+    all_tests = ['tests/TestIcle.c', 'tests/TestYoMama.c']
+
+    redefine_global_constant('COLLECTION_ALL_TESTS', all_tests)
+    redefine_global_constant('PROJECT_TEST_FILE_PREFIX', 'Test')
+    setup()
+
+    # set up expectations
+    @test_invoker.expects.invoke_tests('tests/TestIcle.c')
+    
+    # invoke the task
+    @rake['tests:Icle.c'].invoke
+  end
+
+  should "invoke a test by its source header file name" do
+    all_tests = ['tests/TestIcle.c', 'tests/TestYoMama.c']
+
+    redefine_global_constant('COLLECTION_ALL_TESTS', all_tests)
+    redefine_global_constant('PROJECT_TEST_FILE_PREFIX', 'Test')
+    setup()
+
+    # set up expectations
+    @test_invoker.expects.invoke_tests('tests/TestYoMama.c')
+    
+    # invoke the task
+    @rake['tests:YoMama.h'].invoke
+  end  
+
+  should "complain upon unknown test task" do
+    all_tests = ['tests/TestIcle.c', 'tests/TestYoMama.c']
+
+    redefine_global_constant('COLLECTION_ALL_TESTS', all_tests)
+    redefine_global_constant('PROJECT_TEST_FILE_PREFIX', 'Test')
+    setup()
+
+    # invoke the task
+    assert_raise(RuntimeError){ @rake['tests:Broken.c'].invoke }
+  end  
+
 
 end
