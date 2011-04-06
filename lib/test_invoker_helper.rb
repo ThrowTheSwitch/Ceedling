3faef7e700456969b8e36f60a614b02435db8dab
mkarlesky <mkarlesky@f01913a8-9348-4cf6-9845-091d89bf350d>
2011-04-06 14:27:35 +0000

    documentation update for plugins
    git-svn-id: https://ceedling.svn.sourceforge.net/svnroot/ceedling/trunk@173 f01913a8-9348-4cf6-9845-091d89bf350d

--- lib/test_invoker_helper.rb
+++ lib/test_invoker_helper.rb
@@ -31,7 +31,7 @@ class TestInvokerHelper
   def process_exception(exception)
     if (exception.message =~ /Don't know how to build task '(.+)'/i)
       @streaminator.stderr_puts("ERROR: Rake could not find file referenced in source or test: '#{$1}'.")
-      @streaminator.stderr_puts("Possible stale dependency due to a file name change, etc. Execute 'clean' task and try again.") if (@configurator.project_use_auxiliary_dependencies)
+      @streaminator.stderr_puts("Possible stale dependency due to a file name change, etc. Maybe 'clean' task and try again.") if (@configurator.project_use_auxiliary_dependencies)
       raise ''
     else
       raise exception
