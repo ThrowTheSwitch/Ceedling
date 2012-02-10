89c7bac014ea0740d5651fe4c67e12b460a30295
John Van Enk <vanenkj@gmail.com>
2012-02-10 16:44:28 -0500

    make CI do a bundle install

--- rakefile.rb
+++ rakefile.rb
@@ -1,10 +1,14 @@
 #!/usr/bin/env rake
 require 'bundler'
 
-task :ci => [:spec]
+task :ci => [:bundle_install, :spec]
 
 require 'rspec/core/rake_task'
 desc "Run all rspecs"
 RSpec::Core::RakeTask.new(:spec) do |t|
   t.pattern = 'spec/**/*_spec.rb'
+end
+
+task :bundle_install do
+  sh 'bundle install'
 end
