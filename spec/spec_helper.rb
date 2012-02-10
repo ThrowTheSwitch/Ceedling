59fc8fb97232cf8f9d14ffd0f8342c7233ce3b64
John Van Enk <vanenkj@gmail.com>
2012-02-10 13:29:31 -0500

    Tried require_all, no worky. Maybe later.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,24 @@
 require 'require_all'
 require 'constructor'
 
+RSpec.configure do |configuration|
+  configuration.mock_with :rr
+end
+
 here = File.dirname(__FILE__)
 
 $: << File.join(here, '../lib')
 $: << File.join(here, '../vendor/deep_merge/lib')
+$: << File.join(here, '../vendor/cmock/lib')
 
 support_files = File.join(File.dirname(__FILE__), "support/**/*.rb")
 require_all Dir.glob(support_files)
 support_dir = File.join(File.dirname(__FILE__), 'support')
 
-RSpec.configure do |configuration|
-  configuration.mock_with :rr
-end
+# Eventually, we should use this.
+#
+# # ceedling_files = File.join(File.dirname(__FILE__), '../lib/**/*.rb')
+# # require_all Dir.glob(ceedling_files)
 
 require 'preprocessinator_extractor'
 require 'configurator_builder'
