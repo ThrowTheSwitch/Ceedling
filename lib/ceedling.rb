d1de404d8ceb4776035e89183c87113d9f2f48ae
John Van Enk <vanenkj@gmail.com>
2012-10-01 12:00:45 -0400

    Better error message when there isn't a project.yml in the local directory.

--- lib/ceedling.rb
+++ lib/ceedling.rb
@@ -1,5 +1,10 @@
 require 'rake'
 
+ERR_MSG = <<EOF
+I expected to see a project.yml file in the current directoy. Please create one
+by hand or by using the 'ceedling' shell command.
+EOF
+
 def ceedling_dir
   File.join(
     File.dirname(__FILE__),
@@ -12,7 +17,11 @@ def builtin_ceedling_plugins_path
     'plugins')
 end
 
-load File.join(
-  ceedling_dir,
-  'lib',
-  'rakefile.rb')
+ceeling_lib_rakefile = File.join( ceedling_dir,
+                                 'lib',
+                                 'rakefile.rb')
+if File.exists? "./project.yml"
+  load ceeling_lib_rakefile
+else
+  $stderr.puts ERR_MSG
+end
