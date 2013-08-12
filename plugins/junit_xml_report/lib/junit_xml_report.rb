bce06dfdf62be3852e782a6ba3fa6f3dce017da9
Cormac Cannon <cormacc@gmail.com>
2013-08-12 22:43:27 +0100

    Updated to make more meaningful approximations to package name and class name: - Package name: test source folder - Class name:   test source file base name (i.e. minus ".c" extension, which was confusing jenkins)
    Minor refactoring to reduce duplication.

--- plugins/junit_xml_report/lib/junit_xml_report.rb
+++ plugins/junit_xml_report/lib/junit_xml_report.rb
@@ -44,7 +44,7 @@ class JunitXmlReport < Plugin
 
   def write_results( results, stream )
     write_header( results[:counts], stream ) #includes statistics
-    write_tests( results[:successes], stream)
+    write_successes( results[:successes], stream)
     write_failures( results[:failures], stream )
     write_footer( stream )
   end
@@ -55,29 +55,32 @@ class JunitXmlReport < Plugin
     stream.puts "\t<#{TAGS[:testgroup]} name='Ceedling' tests='#{counts[:total]}' time='0' failures='#{counts[:failed]}' errors='0' skipped='#{counts[:ignored]}'>"
   end
 
-  def write_tests( results, stream )
-
+  def write_successes( results, stream )
     results.each do |result|
-        result[:collection].each do |item|
-          stream.puts "\t\t<#{TAGS[:testcase]} classname='#{File.join( result[:source][:path], result[:source][:file] )}' name='#{item[:test]}'/>"
-          @test_counter += 1
-        end
+      write_result(result, stream, true)
     end
-
   end
 
   def write_failures( results, stream )
-
     results.each do |result|
-        result[:collection].each do |item|
-          filename = File.join( result[:source][:path], result[:source][:file] )
-          stream.puts "\t\t<#{TAGS[:testcase]} classname='#{File.join( result[:source][:path], result[:source][:file] )}' name='#{item[:test]}'>"
-          stream.puts "\t\t\t<#{TAGS[:testfailure]} type='failure' message='#{item[:message]}'>#{filename}:#{item[:line]}</#{TAGS[:testfailure]}>"
-          stream.puts "\t\t</#{TAGS[:testcase]}>"
-          @test_counter += 1
-        end
+      write_result(result, stream, false)
+    end
+  end
+
+  def write_result(result, stream, passed)
+      file_name = File.join( result[:source][:path], result[:source][:file] )
+      class_name = build_class_name( result[:source][:path], result[:source][:file] )
+      result[:collection].each do |item|
+        stream.puts "\t\t<#{TAGS[:testcase]} classname='#{class_name}' name='#{item[:test]}'>"
+        stream.puts "\t\t\t<#{TAGS[:testfailure]} type='failure' message='#{item[:message]}'>#{file_name}:#{item[:line]}</#{TAGS[:testfailure]}>" unless passed
+        stream.puts "\t\t</#{TAGS[:testcase]}>"
+        @test_counter += 1
+      end
     end
 
+  #Turns a file path and file_name.c into a token that Jenkins will interpret as package/classname
+  def build_class_name(file_path,file_name)
+    file_path+"."+file_name.sub('.c','')
   end
 
   def write_footer( stream )
