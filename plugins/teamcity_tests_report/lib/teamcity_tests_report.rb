ccb1fc1939b88ad03da6352eb241751b93186814
Greg Williams <greg.williams@atomicobject.com>
2014-07-04 16:10:53 -0400

    Fixed stdout_ide_tests_report.rb to use proper require paths. Added ability to disable TeamCity outut if TEAMCITY_BUILD constant is defined and is 'true'

--- plugins/teamcity_tests_report/lib/teamcity_tests_report.rb
+++ plugins/teamcity_tests_report/lib/teamcity_tests_report.rb
@@ -2,9 +2,10 @@ require 'ceedling/plugin'
 require 'ceedling/defaults'
 
 class TeamcityTestsReport < Plugin
-  
+
   def setup
     @suite_started = nil
+    @output_enabled = !defined?(TEAMCITY_BUILD) || TEAMCITY_BUILD
   end
 
   def escape(string)
@@ -12,44 +13,45 @@ class TeamcityTestsReport < Plugin
   end
 
   def pre_test(test)
-    puts "##teamcity[testSuiteStarted name='#{File.basename(test, '.c')}']"
+    teamcity_message "testSuiteStarted name='#{File.basename(test, '.c')}'"
     @suite_started = Time.now
   end
 
   def post_test(test)
-    puts "##teamcity[testSuiteFinished name='#{File.basename(test, '.c')}']"
+    teamcity_message "testSuiteFinished name='#{File.basename(test, '.c')}'"
   end
 
   def post_test_fixture_execute(arg_hash)
     duration = (Time.now - @suite_started) * 1000
-
     results = @ceedling[:plugin_reportinator].assemble_test_results([arg_hash[:result_file]])
-
     avg_duration = (duration / [1, results[:counts][:passed] + results[:counts][:failed]].max).round
 
     results[:successes].each do |success|
       success[:collection].each do |test|
-        puts "##teamcity[testStarted name='#{test[:test]}']"
-        puts "##teamcity[testFinished name='#{test[:test]}' duration='#{avg_duration}']"
+        teamcity_message "testStarted name='#{test[:test]}'"
+        teamcity_message "testFinished name='#{test[:test]}' duration='#{avg_duration}'"
       end
     end
 
     results[:failures].each do |failure|
       failure[:collection].each do |test|
-        puts "##teamcity[testStarted name='#{test[:test]}']"
-        puts "##teamcity[testFailed name='#{test[:test]}' message='#{escape(test[:message])}' details='File: #{failure[:source][:path]}/#{failure[:source][:file]} Line: #{test[:line]}']"
-        puts "##teamcity[testFinished name='#{test[:test]}' duration='#{avg_duration}']"
+        teamcity_message "testStarted name='#{test[:test]}'"
+        teamcity_message "testFailed name='#{test[:test]}' message='#{escape(test[:message])}' details='File: #{failure[:source][:path]}/#{failure[:source][:file]} Line: #{test[:line]}'"
+        teamcity_message "testFinished name='#{test[:test]}' duration='#{avg_duration}'"
       end
     end
 
     results[:ignores].each do |failure|
       failure[:collection].each do |test|
-        puts "##teamcity[testIgnored name='#{test[:test]}' message='#{escape(test[:message])}']"
+        teamcity_message "testIgnored name='#{test[:test]}' message='#{escape(test[:message])}'"
       end
     end
 
     # We ignore stdout
   end
 
+  def teamcity_message(content)
+    puts "##teamcity[#{content}]" unless !@output_enabled
+  end
 
 end
