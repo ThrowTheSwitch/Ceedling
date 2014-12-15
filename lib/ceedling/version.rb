3592ab668a0ab344e4e47b71fad0cafa41496581
Greg Williams <greg.williams@atomicobject.com>
2014-12-15 10:11:20 -0500

    Updated cmock and unity to latest Master heads for new release. Bumped Ceedling version.

--- lib/ceedling/version.rb
+++ lib/ceedling/version.rb
@@ -2,15 +2,15 @@
 module Ceedling
   module Version
     # @private
-    GEM = "0.15.6"
+    GEM = "0.16.0"
 
     # @private
     CEEDLING = GEM
     # @private
     CEXCEPTION = "1.3.17"
     # @private
-    CMOCK = "2.0.12"
+    CMOCK = "2.1.0"
     # @private
-    UNITY = "2.1.118"
+    UNITY = "2.2.0"
   end
 end
