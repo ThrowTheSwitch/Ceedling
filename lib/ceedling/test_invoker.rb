20d0f6ef54f64ec37e7b85146debd90e2555bf01
Mark VanderVoord <mvandervoord@gmail.com>
2016-05-06 14:11:38 -0400

    Clobber the executable when we need to rerun a test too.

--- lib/ceedling/test_invoker.rb
+++ lib/ceedling/test_invoker.rb
@@ -22,13 +22,13 @@ class TestInvoker
     @tests   = []
     @mocks   = []
   end
-  
+
   def setup_and_invoke(tests, context=TEST_SYM, options={:force_run => true})
-  
+
     @tests = tests
 
     @project_config_manager.process_test_config_change
-  
+
     @tests.each do |test|
       # announce beginning of test run
       header = "Test '#{File.basename(test)}'"
@@ -36,7 +36,7 @@ class TestInvoker
 
       begin
         @plugin_manager.pre_test( test )
-        
+
         # collect up test fixture pieces & parts
         runner       = @file_path_utils.form_runner_filepath_from_test( test )
         mock_list    = @preprocessinator.preprocess_test_and_invoke_test_mocks( test )
@@ -46,12 +46,12 @@ class TestInvoker
         objects      = @file_path_utils.form_test_build_objects_filelist( [runner] + core + extras )
         results_pass = @file_path_utils.form_pass_results_filepath( test )
         results_fail = @file_path_utils.form_fail_results_filepath( test )
-        
+
         # clean results files so we have a missing file with which to kick off rake's dependency rules
-        @test_invoker_helper.clean_results( {:pass => results_pass, :fail => results_fail}, options )
+        @test_invoker_helper.clean_results( {:pass => results_pass, :fail => results_fail, :test => test }, options )
 
         # load up auxiliary dependencies so deep changes cause rebuilding appropriately
-        @test_invoker_helper.process_deep_dependencies( core ) do |dependencies_list| 
+        @test_invoker_helper.process_deep_dependencies( core ) do |dependencies_list|
           @dependinator.load_test_object_deep_dependencies( dependencies_list )
         end
 
@@ -65,13 +65,13 @@ class TestInvoker
         @dependinator.setup_test_executable_dependencies( test, objects )
 
         # 3, 2, 1... launch
-        @task_invoker.invoke_test_results( results_pass )        
+        @task_invoker.invoke_test_results( results_pass )
       rescue => e
         @build_invoker_utils.process_exception( e, context )
       ensure
         @plugin_manager.post_test( test )
       end
-      
+
       # store away what's been processed
       @mocks.concat( mock_list )
       @sources.concat( sources )
@@ -79,18 +79,18 @@ class TestInvoker
 
     # post-process collected mock list
     @mocks.uniq!
-    
+
     # post-process collected sources list
     @sources.uniq!
   end
 
 
   def refresh_deep_dependencies
-    @file_wrapper.rm_f( 
-      @file_wrapper.directory_listing( 
+    @file_wrapper.rm_f(
+      @file_wrapper.directory_listing(
         File.join( @configurator.project_test_dependencies_path, '*' + @configurator.extension_dependencies ) ) )
 
-    @test_invoker_helper.process_deep_dependencies( 
+    @test_invoker_helper.process_deep_dependencies(
       @configurator.collection_all_tests + @configurator.collection_all_source )
   end
 
