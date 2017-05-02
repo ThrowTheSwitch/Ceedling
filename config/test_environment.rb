3315b5846c0222146a5d303cdba9e23a7dcc98f6
Dom Postorivo <dpostorivo@bulogics.com>
2017-05-02 01:03:42 -0400

    removed constructor from vendor since it has it's own gem

--- config/test_environment.rb
+++ config/test_environment.rb
@@ -5,7 +5,6 @@
   'test',
   'vendor/behaviors/lib',
   'vendor/hardmock/lib',
-  'vendor/constructor/lib',
   'vendor/deep_merge/lib',
 ].each do |dir|
   $LOAD_PATH.unshift( File.join( File.expand_path(File.dirname(__FILE__) + "/../"), dir) )
