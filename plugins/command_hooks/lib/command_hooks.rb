6ecd10aebaa0ea27833ad6e8764b9b4b327a7fb3
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2017-06-14 16:53:08 +0200

    fix(command_hooks) support args
    As described in the readme :)

--- plugins/command_hooks/lib/command_hooks.rb
+++ plugins/command_hooks/lib/command_hooks.rb
@@ -48,7 +48,8 @@ class CommandHooks < Plugin
 
   def run_hook_step(hook, name="")
     if (hook[:executable])
-      cmd = @ceedling[:tool_executor].build_command_line( hook, [], name )
+      args = ( (hook[:args].is_a? Array) ? hook[:args] : [] )
+      cmd = @ceedling[:tool_executor].build_command_line( hook, args, name )
       shell_result = @ceedling[:tool_executor].exec( cmd[:line], cmd[:options] )
     end
   end
