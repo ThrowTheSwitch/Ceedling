2d3af2e685764c541354625e9a49eca440d3cbf8
laurensmiers <miers132@gmail.com>
2017-09-06 20:08:46 +0200

    Add MODULE_GENERATOR_INC_ROOT - Specifies location of created module header files - Not specified by default to not break existing projects   - if needed, specify :inc_root: in *.yml file - + Fix some indentation

--- plugins/module_generator/lib/module_generator.rb
+++ plugins/module_generator/lib/module_generator.rb
@@ -24,16 +24,18 @@ class ModuleGenerator < Plugin
     {
       :path_src     => File.join(module_root_path,
                                  ((defined? MODULE_GENERATOR_SOURCE_ROOT ) ?
-                                    MODULE_GENERATOR_SOURCE_ROOT.gsub('\\', '/').sub(/^\//, '').sub(/\/$/, '')
-                                  : "src" )),
+                                 MODULE_GENERATOR_SOURCE_ROOT.gsub('\\', '/').sub(/^\//, '').sub(/\/$/, '')
+                                 : "src" )),
       :path_inc     => File.join(module_root_path,
-                                 ((defined? MODULE_GENERATOR_SOURCE_ROOT ) ?
-                                    MODULE_GENERATOR_SOURCE_ROOT.gsub('\\', '/').sub(/^\//, '').sub(/\/$/, '')
-                                  : "src" )),
+                                 ((defined? MODULE_GENERATOR_INC_ROOT ) ?
+                                 MODULE_GENERATOR_INC_ROOT.gsub('\\', '/').sub(/^\//, '').sub(/\/$/, '')
+                                 (defined? MODULE_GENERATOR_SOURCE_ROOT ) ?
+                                 MODULE_GENERATOR_SOURCE_ROOT.gsub('\\', '/').sub(/^\//, '').sub(/\/$/, '')
+                                 : "src" )),
       :path_tst     => File.join(module_root_path,
                                  ((defined? MODULE_GENERATOR_TEST_ROOT ) ?
-                                    MODULE_GENERATOR_TEST_ROOT.gsub(  '\\', '/').sub(/^\//, '').sub(/\/$/, '')
-                                  : "test" )),
+                                 MODULE_GENERATOR_TEST_ROOT.gsub(  '\\', '/').sub(/^\//, '').sub(/\/$/, '')
+                                 : "test" )),
       :pattern      => optz[:pattern],
       :test_prefix  => ((defined? PROJECT_TEST_FILE_PREFIX     ) ? PROJECT_TEST_FILE_PREFIX : "Test" ),
       :mock_prefix  => ((defined? CMOCK_MOCK_PREFIX            ) ? CMOCK_MOCK_PREFIX : "Mock" ),
