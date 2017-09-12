ac2146bfa4a86232f2df5ba5c82307ec92eb65dd
Mark VanderVoord <mvandervoord@gmail.com>
2017-09-12 08:38:00 -0400

    Update version information to prepare to post release

--- lib/ceedling/version.rb
+++ lib/ceedling/version.rb
@@ -2,14 +2,14 @@
 module Ceedling
   module Version
     # @private
-    GEM = "0.28.1"
+    GEM = "0.28.2"
     # @private
     CEEDLING = GEM
     # @private
     CEXCEPTION = "1.3.1"
     # @private
-    CMOCK = "2.4.4"
+    CMOCK = "2.4.5"
     # @private
-    UNITY = "2.4.1"
+    UNITY = "2.4.2"
   end
 end
