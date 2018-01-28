5fff51b7a73ae5edf320e113374ed73f70daa74d
Dom Postorivo <dpostorivo@bulogics.com>
2018-01-27 23:00:12 -0500

    timing for junit

--- lib/ceedling/generator_test_results.rb
+++ lib/ceedling/generator_test_results.rb
@@ -1,19 +1,20 @@
 require 'rubygems'
 require 'rake' # for .ext()
 require 'ceedling/constants'
- 
+
 class GeneratorTestResults
 
   constructor :configurator, :generator_test_results_sanity_checker, :yaml_wrapper
- 
+
   def process_and_write_results(unity_shell_result, results_file, test_file)
     output_file   = results_file
-    
+
     results = get_results_structure
-    
+
     results[:source][:path] = File.dirname(test_file)
     results[:source][:file] = File.basename(test_file)
-    
+    results[:time] = unity_shell_result[:time] unless unity_shell_result[:time].nil?
+
     # process test statistics
     if (unity_shell_result[:output] =~ TEST_STDOUT_STATISTICS_PATTERN)
       results[:counts][:total]   = $1.to_i
@@ -24,7 +25,7 @@ class GeneratorTestResults
 
     # remove test statistics lines
     output_string = unity_shell_result[:output].sub(TEST_STDOUT_STATISTICS_PATTERN, '')
-    
+
     output_string.lines do |line|
       # process unity output
       case line
@@ -44,13 +45,13 @@ class GeneratorTestResults
         results[:stdout] << line.chomp
       end
     end
-    
+
     @generator_test_results_sanity_checker.verify(results, unity_shell_result[:exit_code])
-    
+
     output_file = results_file.ext(@configurator.extension_testfail) if (results[:counts][:failed] > 0)
-    
+
     @yaml_wrapper.dump(output_file, results)
-    
+
     return { :result_file => output_file, :result => results }
   end
 
@@ -64,19 +65,20 @@ class GeneratorTestResults
       :ignores   => [],
       :counts    => {:total => 0, :passed => 0, :failed => 0, :ignored  => 0},
       :stdout    => [],
+      :time      => 0.0
       }
   end
-  
+
   def extract_line_elements(line, filename)
     # handle anything preceding filename in line as extra output to be collected
     stdout = nil
     stdout_regex = /(.+)#{Regexp.escape(filename)}.+/i
-    
+
     if (line =~ stdout_regex)
       stdout = $1.clone
       line.sub!(/#{Regexp.escape(stdout)}/, '')
     end
-    
+
     # collect up test results minus and extra output
     elements = (line.strip.split(':'))[1..-1]
 
