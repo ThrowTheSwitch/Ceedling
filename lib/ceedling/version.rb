079a043f469a638f4f4b72d90af774742ef81720
Mark VanderVoord <mvandervoord@gmail.com>
2019-10-21 10:15:31 -0400

    Switching to universal version being in the header file itself.

--- lib/ceedling/version.rb
+++ lib/ceedling/version.rb
@@ -1,15 +1,27 @@
 # @private
 module Ceedling
   module Version
+
+    def version_grabber(filename, prefix)
+      a = [0,0,0]
+      File.readlines(filename) do |line|
+        ["VERSION_MAJOR", "VERSION_MINOR", "VERSION_BUILD"].each_with_index do |field, i|
+          m = line.match(/#{prefix}_#{field}\s+(\d+)/)
+          a[i] = m[1] unless (m.nil?)
+        end
+      end
+      a.join(".")
+    end
+
     # @private
-    GEM = "0.28.4"
+    GEM = "0.29.0"
     # @private
     CEEDLING = GEM
     # @private
-    CEXCEPTION = "1.3.1"
+    CEXCEPTION = version_grabber(File.join(CEXCEPTION_LIB_PATH,CEXCEPTION_H_FILE), "CEXCEPTION")
     # @private
-    CMOCK = "2.4.6"
+    CMOCK = version_grabber( File.join(CMOCK_LIB_PATH,CMOCK_H_FILE), "CMOCK")
     # @private
-    UNITY = "2.4.4"
+    UNITY = version_grabber( File.join(UNITY_LIB_PATH,UNITY_H_FILE), "UNITY")
   end
 end
