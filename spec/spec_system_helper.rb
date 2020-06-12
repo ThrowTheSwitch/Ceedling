bcf0a25c27ee301ec10668a307cf2a8a2ddc95e6
Cezary Gapinski <cezary.gapinski@gmail.com>
2020-06-12 18:56:20 +0200

    Add deployment test for project with enabled preprocessor directives

--- spec/spec_system_helper.rb
+++ spec/spec_system_helper.rb
@@ -72,6 +72,23 @@ def _add_option_in_project(project_file_path, option, value)
   end
 end
 
+def _add_option_in_unity(project_file_path, option, value)
+  project_file_contents = File.readlines(project_file_path)
+  option_index = project_file_contents.index(":unity:\n")
+
+  if option_index.nil?
+    # Silently add before last '...' element
+    project_file_contents.insert(-2, ":unity:\n")
+    option_index = project_file_contents.index(":unity:\n")
+  end
+
+  project_file_contents.insert(option_index + 1, "  :#{option}: #{value}\n")
+
+  File.open(project_file_path, "w+") do |f|
+    f.puts(project_file_contents)
+  end
+end
+
 def add_source_path(path)
   _add_path_in_section("project.yml", path, "source")
 end
@@ -86,6 +103,10 @@ end
 
 def add_project_option(option, value)
   _add_option_in_project("project.yml", option, value)
+end
+
+def add_unity_option(option, value)
+  _add_option_in_unity("project.yml", option, value)
 end
 
 def add_module_generator_section(project_file_path, mod_gen)
@@ -362,6 +383,23 @@ module CeedlingTestCases
         FileUtils.copy_entry test_asset_path("auto_link_deep_dependencies/src/"), 'src/'
         FileUtils.cp_r test_asset_path("auto_link_deep_dependencies/test/."), 'test/'
         add_project_option("auto_link_deep_dependencies", "TRUE")
+
+        output = `bundle exec ruby -S ceedling 2>&1`
+        expect($?.exitstatus).to match(0) # Since a test either pass or are ignored, we return success here
+        expect(output).to match(/TESTED:\s+\d/)
+        expect(output).to match(/PASSED:\s+\d/)
+        expect(output).to match(/FAILED:\s+\d/)
+        expect(output).to match(/IGNORED:\s+\d/)
+      end
+    end
+  end
+
+  def can_test_projects_with_enabled_preprocessor_directives_with_success
+    @c.with_context do
+      Dir.chdir @proj_name do
+        FileUtils.cp test_asset_path("test_example_with_parameterized_tests.c"), 'test/'
+        add_project_option("use_preprocessor_directives", "TRUE")
+        add_unity_option("use_param_tests", "TRUE")
 
         output = `bundle exec ruby -S ceedling 2>&1`
         expect($?.exitstatus).to match(0) # Since a test either pass or are ignored, we return success here
