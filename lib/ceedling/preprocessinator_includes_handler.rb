b398236f097f491a56f29f95e819a29a430582ff
Cezary Gapinski <cezary.gapinski@gmail.com>
2021-02-13 15:20:18 +0100

    Ignore real header if also mock exists
    When both real header and mock exists, the mock should have priority. Situation is more complex with nested headers when deep dependencies are needed.

--- lib/ceedling/preprocessinator_includes_handler.rb
+++ lib/ceedling/preprocessinator_includes_handler.rb
@@ -108,15 +108,30 @@ class PreprocessinatorIncludesHandler
     full_path_sources_dependencies = extract_full_path_dependencies(sources_dependencies)
 
     list = full_path_headers_dependencies + full_path_sources_dependencies
+
+    mock_prefix = @configurator.project_config_hash[:cmock_mock_prefix]
+    # Creating list of mocks
+    mocks += full_path_headers_dependencies.find_all do |header|
+      File.basename(header) =~ /^#{mock_prefix}.*$/
+    end.compact
+
+    # ignore real file when both mock and real file exist
+    mocks.each do |mock|
+      list.each do |filename|
+        if File.basename(filename) == File.basename(mock).sub(mock_prefix, '')
+          ignore_list << filename
+        end
+      end
+    end.compact
+
+    # Filtering list of final includes to only include mocks and anything that is NOT in the ignore_list
+    list = list.select do |item|
+      mocks.include? item or !(ignore_list.any? { |ignore_item| !item.match(/^(.*\/)?#{Regexp.escape(ignore_item)}$/).nil? })
+    end
+
     to_process = []
 
     if @configurator.project_config_hash[:project_auto_link_deep_dependencies]
-      mock_prefix = @configurator.project_config_hash[:cmock_mock_prefix]
-      # Creating list of mocks
-      mocks += full_path_headers_dependencies.find_all do |header|
-        File.basename(header) =~ /^#{mock_prefix}.*$/
-      end.compact
-
       # Creating list of headers that should be recursively pre-processed
       # Skipping mocks and vendor headers
       headers_to_deep_link = full_path_headers_dependencies.select do |hdr|
@@ -124,20 +139,6 @@ class PreprocessinatorIncludesHandler
       end
       headers_to_deep_link.map! {|hdr| File.expand_path(hdr) }
       headers_to_deep_link.compact!
-
-      # ignore real file when both mock and real file exist
-      mocks.each do |mock|
-        list.each do |filename|
-          if File.basename(filename) == File.basename(mock).sub(mock_prefix, '')
-            ignore_list << filename
-          end
-        end
-      end.compact
-
-      # Filtering list of final includes to only include mocks and anything that is NOT in the ignore_list
-      list = list.select do |item|
-        mocks.include? item or !(ignore_list.any? { |ignore_item| !item.match(/^(.*\/)?#{Regexp.escape(ignore_item)}$/).nil? })
-      end
 
       headers_to_deep_link.each do |hdr|
         if (ignore_list.none? {|ignore_header| hdr.match(/^(.*\/)?#{Regexp.escape(ignore_header)}$/)} and
