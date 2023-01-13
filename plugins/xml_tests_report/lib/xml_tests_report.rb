ThrowTheSwitch/Ceedling

commit 2b42ea6ac1cc606d401ebe5384585a9887808e0c

Report test file name including subfolders

plugins/xml_tests_report/lib/xml_tests_report.rb
@@ -56,7 +56,7 @@ class XmlTestsReport < Plugin
 
     results.each do |result|
       result[:collection].each do |item|
-        filename = File.join(result[:source][:path], result[:source][:file])
+        filename = result[:source][:file]
 
         stream.puts "\t\t<Test id=\"#{@test_counter}\">"
         stream.puts "\t\t\t<Name>#{filename}::#{item[:test]}</Name>"

lib/ceedling/defaults.rb
@@ -422,7 +422,7 @@ DEFAULT_TESTS_RESULTS_REPORT_TEMPLATE = %q{
 <%=@ceedling[:plugin_reportinator].generate_banner(header_prepend + 'TEST OUTPUT')%>
 %   hash[:results][:stdout].each do |string|
 %     string[:collection].each do |item|
-<%=string[:source][:path]%><%=File::SEPARATOR%><%=string[:source][:file]%>: "<%=item%>"
+<%=string[:source][:file]%>: "<%=item%>"
 %     end
 %   end
 
@@ -431,7 +431,7 @@ DEFAULT_TESTS_RESULTS_REPORT_TEMPLATE = %q{
 <%=@ceedling[:plugin_reportinator].generate_banner(header_prepend + 'IGNORED TEST SUMMARY')%>
 %   hash[:results][:ignores].each do |ignore|
 %     ignore[:collection].each do |item|
-<%=ignore[:source][:path]%><%=File::SEPARATOR%><%=ignore[:source][:file]%>:<%=item[:line]%>:<%=item[:test]%>
+<%=ignore[:source][:file]%>:<%=item[:line]%>:<%=item[:test]%>
 % if (item[:message].length > 0)
 : "<%=item[:message]%>"
 % else
@@ -445,7 +445,7 @@ DEFAULT_TESTS_RESULTS_REPORT_TEMPLATE = %q{
 <%=@ceedling[:plugin_reportinator].generate_banner(header_prepend + 'FAILED TEST SUMMARY')%>
 %   hash[:results][:failures].each do |failure|
 %     failure[:collection].each do |item|
-<%=failure[:source][:path]%><%=File::SEPARATOR%><%=failure[:source][:file]%>:<%=item[:line]%>:<%=item[:test]%>
+<%=failure[:source][:file]%>:<%=item[:line]%>:<%=item[:test]%>
 % if (item[:message].length > 0)
 : "<%=item[:message]%>"
 % else

lib/ceedling/generator_test_results.rb
@@ -11,8 +11,9 @@ class GeneratorTestResults
 
     results = get_results_structure
 
-    results[:source][:path] = File.dirname(test_file)
-    results[:source][:file] = File.basename(test_file)
+    results[:source][:dirname] = File.dirname(test_file)
+    results[:source][:basename] = File.basename(test_file)
+    results[:source][:file] = test_file
     results[:time] = unity_shell_result[:time] unless unity_shell_result[:time].nil?
 
     # process test statistics
@@ -63,7 +64,7 @@ class GeneratorTestResults
 
   def get_results_structure
     return {
-      :source    => {:path => '', :file => ''},
+      :source    => {:file => '', :dirname => '', :basename => '' },
       :successes => [],
       :failures  => [],
       :ignores   => [],

lib/ceedling/test_invoker.rb
@@ -50,7 +50,7 @@ class TestInvoker
 
     @tests.each do |test|
       # announce beginning of test run
-      header = "Test '#{File.basename(test)}'"
+      header = "Test '#{test}'"
       @streaminator.stdout_puts("\n\n#{header}\n#{'-' * header.length}")
 
       begin

plugins/json_tests_report/lib/json_tests_report.rb
@@ -47,7 +47,7 @@ class JsonTestsReport < Plugin
       result[:collection].each do |item|
         @test_counter += 1
         retval << {
-          "file" => File.join(result[:source][:path], result[:source][:file]),
+          "file" => result[:source][:file],
           "test" => item[:test],
           "line" => item[:line],
           "message" => item[:message]
@@ -63,7 +63,7 @@ class JsonTestsReport < Plugin
       result[:collection].each do |item|
         @test_counter += 1
         retval << { 
-          "file" => File.join(result[:source][:path], result[:source][:file]),
+          "file" => result[:source][:file],
           "test" => item[:test]
         }
       end

plugins/teamcity_tests_report/lib/teamcity_tests_report.rb
@@ -36,7 +36,7 @@ class TeamcityTestsReport < Plugin
     results[:failures].each do |failure|
       failure[:collection].each do |test|
         teamcity_message "testStarted name='#{test[:test]}'"
-        teamcity_message "testFailed name='#{test[:test]}' message='#{escape(test[:message])}' details='File: #{failure[:source][:path]}/#{failure[:source][:file]} Line: #{test[:line]}'"
+        teamcity_message "testFailed name='#{test[:test]}' message='#{escape(test[:message])}' details='File: #{failure[:source][:file]} Line: #{test[:line]}'"
         teamcity_message "testFinished name='#{test[:test]}' duration='#{avg_duration}'"
       end
     end

commit 2117171f689e2c6e062ec03c5bfcdd85eaa2baf1

Fix exception in xml report introduced by commit 2b42ea6

plugins/xml_tests_report/lib/xml_tests_report.rb
@@ -84,8 +84,9 @@ class XmlTestsReport < Plugin
 
     results.each do |result|
       result[:collection].each do |item|
+ 		filename = result[:source][:file]
         stream.puts "\t\t<Test id=\"#{@test_counter}\">"
-        stream.puts "\t\t\t<Name>#{File.join(result[:source][:path], result[:source][:file])}::#{item[:test]}</Name>"
+        stream.puts "\t\t\t<Name>#{filename}::#{item[:test]}</Name>"
         stream.puts "\t\t</Test>"
         @test_counter += 1
       end