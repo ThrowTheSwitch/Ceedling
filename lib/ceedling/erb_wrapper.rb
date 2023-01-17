ca1a08d98467fe9e645713fa85945ee5bc30573c
Łukasz Żegliński <lukasz.zeglinski@mobica.com>
2023-01-17 10:30:02 +0100

    Add trim_mode to ERB.new parameter specifier

--- lib/ceedling/erb_wrapper.rb
+++ lib/ceedling/erb_wrapper.rb
@@ -3,7 +3,7 @@ require 'erb'
 class ErbWrapper
   def generate_file(template, data, output_file)
     File.open(output_file, "w") do |f|
-      f << ERB.new(template, 0, "<>").result(binding)
+      f << ERB.new(template, trim_mode: "<>").result(binding)
     end
   end
 end
