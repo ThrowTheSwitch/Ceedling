e252eed3bcd5bfc2d5db63272c8fed3f44c5b944
Mark VanderVoord <mvandervoord@gmail.com>
2023-12-12 22:57:38 -0500

    whoops. need to fix that last one.

--- lib/ceedling/test_invoker.rb
+++ lib/ceedling/test_invoker.rb
@@ -245,7 +245,7 @@ class TestInvoker
           test_core          = test_sources + details[:mock_list]
 
           # When we have a mock and an include for the same file, the mock wins
-          test_core.delete_if {|v| details[:mock_list].include?("#{CMOCK_MOCK_PREFIX}#{File.basename(v,'.*')}#{CMOCK_MOCK_SUFFIX}#{EXTENSION_SOURCE}")}
+          #test_core.delete_if {|v| details[:mock_list].include?("#{CMOCK_MOCK_PREFIX}#{File.basename(v,'.*')}#{CMOCK_MOCK_SUFFIX}#{EXTENSION_SOURCE}")}
           
           # CMock + Unity + CException
           test_frameworks    = @helper.collect_test_framework_sources
