0f691e32bafb84d9c0dab822de85d1c2e907e16d
Mike Karlesky <michael@karlesky.net>
2024-01-04 16:11:52 -0500

    IDE Test Report Plugin updates
    - Added mutex for post test fixture event handling
    - Made documentation more better

--- plugins/stdout_ide_tests_report/lib/stdout_ide_tests_report.rb
+++ plugins/stdout_ide_tests_report/lib/stdout_ide_tests_report.rb
@@ -5,40 +5,60 @@ class StdoutIdeTestsReport < Plugin
 
   def setup
     @result_list = []
+    @mutex = Mutex.new
+
+    # Set the report template (which happens to be the Ceedling default)
+    @ceedling[:plugin_reportinator].register_test_results_template(
+      DEFAULT_TESTS_RESULTS_REPORT_TEMPLATE
+      )
   end
 
+  # Collect result file paths after each test fixture execution
   def post_test_fixture_execute(arg_hash)
-    return if not (arg_hash[:context] == TEST_SYM)
-
-    @result_list << arg_hash[:result_file]
+    # Thread-safe manipulation since test fixtures can be run in child processes
+    # spawned within multiple test execution threads.
+    @mutex.synchronize do
+      @result_list << arg_hash[:result_file]
+    end
   end
 
-  def post_build
+  # Render a report immediately upon build completion (that invoked tests)
+  def post_build()
+    # Ensure a test task was invoked as part of the build
     return if (not @ceedling[:task_invoker].test_invoked?)
 
-    results = @ceedling[:plugin_reportinator].assemble_test_results(@result_list)
+    results = @ceedling[:plugin_reportinator].assemble_test_results( @result_list )
     hash = {
       :header => '',
       :results => results
     }
 
-    @ceedling[:plugin_reportinator].run_test_results_report(hash) do
+    @ceedling[:plugin_reportinator].run_test_results_report( hash ) do
       message = ''
       message = 'Unit test failures.' if (hash[:results][:counts][:failed] > 0)
       message
     end
   end
 
-  def summary
-    result_list = @ceedling[:file_path_utils].form_pass_results_filelist( PROJECT_TEST_RESULTS_PATH, COLLECTION_ALL_TESTS )
+  # Render a test results report on demand using results from a previous build
+  def summary()
+    # Build up the list of passing results from all tests
+    result_list = @ceedling[:file_path_utils].form_pass_results_filelist(
+      PROJECT_TEST_RESULTS_PATH,
+      COLLECTION_ALL_TESTS
+    )
 
-    # get test results for only those tests in our configuration and of those only tests with results on disk
     hash = {
       :header => '',
-      :results => @ceedling[:plugin_reportinator].assemble_test_results(result_list, {:boom => false})
+      # Collect all existing test results (success or failing) in the filesystem,
+      # limited to our test collection
+      :results => @ceedling[:plugin_reportinator].assemble_test_results(
+        result_list,
+        {:boom => false}
+      )
     }
 
-    @ceedling[:plugin_reportinator].run_test_results_report(hash)
+    @ceedling[:plugin_reportinator].run_test_results_report( hash )
   end
 
 end
