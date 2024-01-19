7ac58166ab715c54e0de08fbca6c14f141ddccf8
Mike Karlesky <michael@karlesky.net>
2024-01-18 21:16:58 -0500

    Used new tool validation

--- plugins/beep/lib/beep.rb
+++ plugins/beep/lib/beep.rb
@@ -12,10 +12,11 @@ class Beep < Plugin
     
     # Get beep configuration hash
     beep_config = project_config[BEEP_SYM]
+
     # Get tools hash
     tools = project_config[:tools]
 
-    # Lookup the selected beep tool
+    # Lookup and capture the selected beep tools
     @tools = {
       :beep_on_done => tools["beep_#{beep_config[:on_done]}".to_sym],
       :beep_on_error => tools["beep_#{beep_config[:on_error]}".to_sym]
@@ -23,21 +24,32 @@ class Beep < Plugin
     
     # Ensure configuration option is an actual tool
     if @tools[:beep_on_done].nil?
-      raise CeedlingException.new("Option :#{beep_config[:on_done]} for :beep ↳ :on_done plugin configuration does not map to a tool.")
+      error = "Option :#{beep_config[:on_done]} for :beep ↳ :on_done plugin configuration does not map to a tool."
+      raise CeedlingException.new( error )
     end
     
     # Ensure configuration option is an actual tool
     if @tools[:beep_on_error].nil?
-      raise CeedlingException.new("Option :#{beep_config[:on_done]} for :beep ↳ :on_error plugin configuration does not map to a tool.")
+      error = "Option :#{beep_config[:on_done]} for :beep ↳ :on_error plugin configuration does not map to a tool."
+      raise CeedlingException.new( error )
     end
+
+    # Validate the selected beep tools
+    # Do not validate the `:bell` tool as it relies on `echo` that could be a shell feature rather than executable
+    @ceedling[:tool_validator].validate(
+      tool: @tools[:beep_on_done],
+      extension: EXTENSION_EXECUTABLE,
+      boom: true
+    ) if tools[:on_done] != :bell
+
+    @ceedling[:tool_validator].validate(
+      tool: @tools[:beep_on_error],
+      extension: EXTENSION_EXECUTABLE,
+      boom: true
+    ) if tools[:on_error] != :bell
   end
   
   def post_build
-    if @tools[:beep_on_done].nil?
-      @ceedling[:streaminator].stderr_puts("Tool for :beep ↳ :on_done event handling is not available", Verbosity::COMPLAIN)
-      return
-    end
-
     command = @ceedling[:tool_executor].build_command_line(
       @tools[:beep_on_done],
       [],
@@ -50,11 +62,6 @@ class Beep < Plugin
   end
   
   def post_error
-    if @tools[:beep_on_error].nil?
-      @ceedling[:streaminator].stderr_puts("Tool for :beep ↳ :on_error event handling is not available", Verbosity::COMPLAIN)
-      return
-    end
-
     command = @ceedling[:tool_executor].build_command_line(
       @tools[:beep_on_error],
       [],
