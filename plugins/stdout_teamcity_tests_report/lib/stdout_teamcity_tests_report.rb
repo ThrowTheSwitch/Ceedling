73853862c78017df3304f77e7f6e481a53afff8d
Mike Karlesky <michael@karlesky.net>
2024-01-31 12:04:54 -0500

    Replaced time handling with test results time
    - Test execution duration was being fully calculated, but it was already available in test results
    - Added improved documentation

--- plugins/stdout_teamcity_tests_report/lib/stdout_teamcity_tests_report.rb
+++ plugins/stdout_teamcity_tests_report/lib/stdout_teamcity_tests_report.rb
@@ -4,33 +4,34 @@ require 'ceedling/defaults'
 class StdoutTeamcityTestsReport < Plugin
 
   def setup
-    @suite_started = nil
-
-    # TEAMCITY_BUILD defaults to true but can be overridden in a user project file to stop CI messages locally
+    # TEAMCITY_BUILD defaults to true but can be overridden in a user 
+    # project file to stop CI messages locally.
     @output_enabled = TEAMCITY_BUILD
 
+    # Provide thread-safety for multi-threaded builds
     @mutex = Mutex.new
 
-    @flowid_count = 1
+    # A counter incremented for each Ceedling test executable allowing 
+    # concurrent executables to differentiate their service messages.
+    @flowid_count = 0
 
-    # A TeamCity suite correlates to a test executable
-    # This hash tracks:
-    #  - Suite start time
-    #  - Flow ID (to differentiate tests running in concurrent threads)
+    # A TeamCity suite correlates to a Ceedling test executable
+    # This hash relates each test filepath to a unique Flow ID
+    # (TeamCity uses Flow IDs to differentiate messages generated in concurrent threads)
     @suites = {}
   end
 
+  # Hook run before each test executable begins building
   def pre_test(test)
     return if !@output_enabled
 
+    # Generate a new Flow ID and store it in test hash
     @mutex.synchronize do
-      @suites[test] = {
-        :started => Time.now(),
-        :flowid => @flowid_count
-      }
       @flowid_count += 1
+      @suites[test] = { :flowid => @flowid_count }
     end
 
+    # Generate first TeamCity service message
     @mutex.synchronize do
       teamcity_service_message(
         "testSuiteStarted name='#{File.basename(test, '.*')}'",
@@ -39,48 +40,43 @@ class StdoutTeamcityTestsReport < Plugin
     end
   end
 
-  def post_test(test)
-    return if !@output_enabled
-
-    @mutex.synchronize do
-      teamcity_service_message(
-        "testSuiteFinished name='#{File.basename(test, '.*')}'",
-        @suites[test][:flowid]
-      )
-    end
-  end
-
+  # Hook run after test executable is run
   def post_test_fixture_execute(arg_hash)
     return if !@output_enabled
 
-    _duration_ms = nil
-    _flowId = nil
-    test_key = arg_hash[:test]
+    flowId = nil
+    test_key = arg_hash[:test_filepath]
 
+    # Get unique Flow ID
     @mutex.synchronize do
-      _duration_ms = (Time.now() - @suites[test_key][:started]) * 1000
-      _flowId = @suites[test_key][:flowid]
+      flowId = @suites[test_key][:flowid]
     end
 
-    results = @ceedling[:plugin_reportinator].assemble_test_results([arg_hash[:result_file]])
-    avg_duration = (_duration_ms / [1, results[:counts][:passed] + results[:counts][:failed]].max).round
+    # Gather test results for this test executable
+    results = @ceedling[:plugin_reportinator].assemble_test_results( [arg_hash[:result_file]] )
 
+    # Apportion total run time of test executable equally to each test case within it.
+    duration_ms = results[:total_time] * 1000.0
+    avg_duration = (duration_ms / [1, results[:counts][:passed] + results[:counts][:failed]].max).round
+
+    # Handle test case successes within the test executable
     results[:successes].each do |success|
       success[:collection].each do |test|
         _test = test[:test]
 
         teamcity_service_message(
           "testStarted name='#{_test}'",
-          _flowId
+          flowId
         )
 
         teamcity_service_message(
           "testFinished name='#{_test}' duration='#{avg_duration}'",
-          _flowId
+          flowId
         )
       end
     end
 
+    # Handle test case failures within the test executable
     results[:failures].each do |failure|
       failure[:collection].each do |test|
         _test = test[:test]
@@ -88,24 +84,24 @@ class StdoutTeamcityTestsReport < Plugin
 
         teamcity_service_message(
           "testStarted name='#{_test}'",
-          _flowId
+          flowId
         )
 
         _message = 
           "testFailed name='#{_test}' " + 
-          # Always a message in a test failure
           "message='#{escape(_message)}' " +
           "details='File: #{failure[:source][:file]} Line: #{test[:line]}'"
 
-        teamcity_service_message( _message, _flowId )
+        teamcity_service_message( _message, flowId )
 
         teamcity_service_message(
           "testFinished name='#{_test}' duration='#{avg_duration}'",
-          _flowId
+          flowId
         )
       end
     end
 
+    # Handle ignored tests
     results[:ignores].each do |failure|
       failure[:collection].each do |test|
         _test = test[:test]
@@ -118,15 +114,29 @@ class StdoutTeamcityTestsReport < Plugin
 
   end
 
+  # Hook run after a test executable build
+  def post_test(test)
+    return if !@output_enabled
+
+    @mutex.synchronize do
+      teamcity_service_message(
+        "testSuiteFinished name='#{File.basename(test, '.*')}'",
+        @suites[test][:flowid]
+      )
+    end
+  end
+
   ### Private
 
   private
 
   def escape(string)
+    # https://www.jetbrains.com/help/teamcity/service-messages.html#Escaped+Values
     string.gsub(/['|\[\]]/, '|\0').gsub('\r', '|r').gsub('\n', '|n')
   end
 
   def teamcity_service_message(content, flowId=0)
+    # https://www.jetbrains.com/help/teamcity/service-messages.html
     puts "##teamcity[#{content} flowId='#{flowId}']"
   end
 
