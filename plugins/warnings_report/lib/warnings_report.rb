234db2e9783d3b60ab0da620181aa42a58c05e6f
Mike Karlesky <michael@karlesky.net>
2024-02-22 14:27:06 -0500

    Updated warnings report plugin
    - Removed dangerous / unreliable $stderr redirect override
    - Refactored for thread-safety for multi-threaded Ceedling and more efficient file handling
    - Added preprocessing build hook handling
    - Added logging

--- plugins/warnings_report/lib/warnings_report.rb
+++ plugins/warnings_report/lib/warnings_report.rb
@@ -3,67 +3,125 @@ require 'ceedling/constants'
 
 class WarningsReport < Plugin
   def setup
-    @stderr_redirect = nil
-    @log_paths = {}
+    # Create structure of @warnings hash with default values
+    @warnings = Hash.new() do |h,k|
+      # k => :context
+      h[k] = {
+        collection: [],
+      }
+    end
+
+    # Ceedling can run with multiple threads, provide a lock to use around @warnings
+    @mutex = Mutex.new()
+
+    # Get default (default.yml) / user-set log filename in project.yml
+    @log_filename = @ceedling[:configurator].warnings_report_filename
+
+    # Convenient instance variable references
+    @file_wrapper = @ceedling[:file_wrapper]
+    @streaminator = @ceedling[:streaminator]
+    @reportinator = @ceedling[:reportinator]
   end
 
-  def pre_compile_execute(arg_hash)
-    # at beginning of compile, override tool's stderr_redirect so we can parse $stderr + $stdout
-    set_stderr_redirect(arg_hash)
+  # Build step hook
+  def post_mock_preprocess(arg_hash)
+    # After preprocessing, parse output, store warning if found
+    process_output(
+      arg_hash[:context],
+      arg_hash[:shell_result][:output],
+      @warnings
+    )
   end
 
+  # Build step hook
+  def post_test_preprocess(arg_hash)
+    # After preprocessing, parse output, store warning if found
+    process_output(
+      arg_hash[:context],
+      arg_hash[:shell_result][:output],
+      @warnings
+    )
+  end
+
+  # Build step hook
   def post_compile_execute(arg_hash)
-    # after compilation, grab output for parsing/logging, restore stderr_redirect, log warning if it exists
-    output = arg_hash[:shell_result][:output]
-    restore_stderr_redirect(arg_hash)
-    write_warning_log(arg_hash[:context], output)
+    # After compiling, parse output, store warning if found
+    process_output(
+      arg_hash[:context],
+      arg_hash[:shell_result][:output],
+      @warnings
+    )
   end
 
-  def pre_link_execute(arg_hash)
-    # at beginning of link, override tool's stderr_redirect so we can parse $stderr + $stdout
-    set_stderr_redirect(arg_hash)
-  end
-
+  # Build step hook
   def post_link_execute(arg_hash)
-    # after linking, grab output for parsing/logging, restore stderr_redirect, log warning if it exists
-    output = arg_hash[:shell_result][:output]
-    restore_stderr_redirect(arg_hash)
-    write_warning_log(arg_hash[:context], output)
+    # After linking, parse output, store warning if found
+    process_output(
+      arg_hash[:context],
+      arg_hash[:shell_result][:output],
+      @warnings
+    )
   end
+
+  # Build step hook
+  def post_build()
+    # Write collected warnings to log(s)
+    write_logs( @warnings, @log_filename )
+  end
+
+  # Build step hook
+  def post_error()
+    # Write collected warnings to log(s)
+    write_logs( @warnings, @log_filename )
+  end
+
+  ### Private ###
 
   private
 
-  def set_stderr_redirect(hash)
-    @stderr_redirect = hash[:tool][:stderr_redirect]
-    hash[:tool][:stderr_redirect] = StdErrRedirect::AUTO
-  end
+  # Extract warning messages and store to hash in thread-safe manner
+  def process_output(context, output, hash)
+    # If $stderr/$stdout does not contain "warning", bail out
+    return if !(output =~ /warning/i)
 
-  def restore_stderr_redirect(hash)
-    hash[:tool][:stderr_redirect] = @stderr_redirect
-  end
-
-  def write_warning_log(context, output)
-    # if $stderr/$stdout contain "warning", log it
-    if output =~ /warning/i
-      # generate a log path & file io write flags
-      logging = generate_log_path(context)
-      @ceedling[:file_wrapper].write(logging[:path], output + "\n", logging[:flags]) unless logging.nil?
+    # Store warning message
+    @mutex.synchronize do
+      hash[context][:collection] << output
     end
   end
 
-  def generate_log_path(context)
-    # if path has already been generated, return it & 'append' file io flags (append to log)
-    return { path: @log_paths[context], flags: 'a' } unless @log_paths[context].nil?
+  # Walk warnings hash and write contents to log file(s)
+  def write_logs( warnings, filename )
+    msg = @reportinator.generate_heading( "Running Warnings Report" )
+    @streaminator.stdout_puts( msg )
 
-    # first time through, generate path & 'write' file io flags (create new log)
-    base_path = File.join(PROJECT_BUILD_ARTIFACTS_ROOT, context.to_s)
-    file_path = File.join(base_path, 'warnings.log')
-
-    if @ceedling[:file_wrapper].exist?(base_path)
-      @log_paths[context] = file_path
-      return { path: file_path, flags: 'w' }
+    if warnings.empty?
+      @streaminator.stdout_puts( "No build warnings collected.\n" )
+      return
     end
 
-    nil
+    warnings.each do |context, hash|
+      log_filepath = form_log_filepath( context, filename )
+
+      msg = @reportinator.generate_progress( "Generating artifact #{log_filepath}" )
+      @streaminator.stdout_puts( msg )
+
+      File.open( log_filepath, 'w' ) do |f|
+        hash[:collection].each { |warning| f << warning }
+      end
+    end
+
+    # White space at command line after progress messages
+    @streaminator.stdout_puts( '' )
+  end
+
+  def form_log_filepath(context, filename)
+    path = File.join( PROJECT_BUILD_ARTIFACTS_ROOT, context.to_s )
+    filepath = File.join(path, filename)
+
+    # Ensure containing artifact directory exists
+    @file_wrapper.mkdir( path )
+
+    return filepath
   end
 end
