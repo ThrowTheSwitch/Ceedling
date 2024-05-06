359f608fe5d5ef897a3ff6118593c2dc33c26c84
Mike Karlesky <michael@karlesky.net>
2024-05-06 11:22:13 -0400

    📦️ Another gem dependency fix attempt

--- spec/reportinator_spec.rb
+++ spec/reportinator_spec.rb
@@ -5,24 +5,22 @@
 #   SPDX-License-Identifier: MIT
 # =========================================================================
 
-# Temporarily disabled until Unicode gem dependency needed by this test is resolved
+require 'spec_helper'
+require 'ceedling/reportinator'
 
-# require 'spec_helper'
-# require 'ceedling/reportinator'
-
-# describe Reportinator do
-#   before(:each) do
-#     @rp = described_class.new
-#   end
+describe Reportinator do
+  before(:each) do
+    @rp = described_class.new
+  end
  
-#   describe '#generate_banner' do
-#     it 'generates a banner with a width based on a string' do
-#       expect(@rp.generate_banner("Hello world!")).to eq("------------\nHello world!\n------------\n")
-#     end
+  describe '#generate_banner' do
+    it 'generates a banner with a width based on a string' do
+      expect(@rp.generate_banner("Hello world!")).to eq("------------\nHello world!\n------------\n")
+    end
 
-#     it 'generates a banner with a fixed width' do
-#       expect(@rp.generate_banner("Hello world!", 3)).to eq("---\nHello world!\n---\n")
-#     end
-#   end
+    it 'generates a banner with a fixed width' do
+      expect(@rp.generate_banner("Hello world!", 3)).to eq("---\nHello world!\n---\n")
+    end
+  end
 
-# end
+end
