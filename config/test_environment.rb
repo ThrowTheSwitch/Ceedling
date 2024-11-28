6061ccfc3a68015673f2e36d702f893e839234b9
Michael Karlesky <michael@karlesky.net>
2024-11-28 13:58:33 -0500

    🔥 Removed unused vendored behaviors & hardmock

--- config/test_environment.rb
+++ config/test_environment.rb
@@ -9,8 +9,6 @@
 [ 
   'lib',
   'test',
-  'vendor/behaviors/lib',
-  'vendor/hardmock/lib',
 ].each do |dir|
   $LOAD_PATH.unshift( File.join( File.expand_path(File.dirname(__FILE__) + "/../"), dir) )
 end
