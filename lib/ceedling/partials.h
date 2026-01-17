09d65cbd834ad0d619e0be209981474ba834d625
Michael Karlesky <michael@karlesky.net>
2026-01-17 16:05:39 -0500

    ✨ Partials automatic configuration (in progress)

--- lib/ceedling/partials.h
+++ lib/ceedling/partials.h
@@ -2,8 +2,8 @@
 #define CEEDLING_PARTIALS_H
 
 // Define mock prefix via command line symbol definition
-#ifndef CEEDLING_MOCK_PREFIX
-#define CEEDLING_MOCK_PREFIX mock_
+#ifndef CMOCK_MOCK_PREFIX
+#define CMOCK_MOCK_PREFIX mock_
 #endif
 
 // Stringification and tokenization helper macros
@@ -14,7 +14,15 @@
 // Create a unique namespaced variable name
 #define PARTIAL_LOCAL_VAR(namespace, var) partial_##namespace##_##var
 
-#define TEST_PARTIAL_MODULE(file, ...) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_impl.h))
-#define MOCK_PARTIAL_MODULE(file, ...) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CEEDLING_MOCK_PREFIX)__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_interface.h))
+// Candidate eventual partials macros
+// #define TEST_PARTIAL_MODULE(file, ...) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_impl.h))
+// #define MOCK_PARTIAL_MODULE(file, ...) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CMOCK_MOCK_PREFIX)__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_interface.h))
+
+// Temporary, first working version partials macros
+#define TEST_PARTIAL_PUBLIC_MODULE(file) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_impl.h))
+#define TEST_PARTIAL_PRIVATE_MODULE(file) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_impl.h))
+#define MOCK_PARTIAL_PRIVATE_MODULE(file) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CMOCK_MOCK_PREFIX)__PARTIALS_EXPAND(partial_)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(_interface.h))
+#define MOCK_PARTIAL_PUBLIC_MODULE(file) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CMOCK_MOCK_PREFIX)__PARTIALS_EXPAND(file)__PARTIALS_EXPAND(.h))
+
 
 #endif /* CEEDLING_PARTIALS_H */
