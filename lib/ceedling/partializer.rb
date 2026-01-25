d94e65011f010be08f3ec968683da177ce563ea1
Michael Karlesky <michael@karlesky.net>
2026-01-24 20:37:59 -0500

    ♻️ Reorganize structs and factory methods

--- lib/ceedling/partializer.rb
+++ lib/ceedling/partializer.rb
@@ -1,3 +1,10 @@
+# =========================================================================
+#   Ceedling - Test-Centered Build System for C
+#   ThrowTheSwitch.org
+#   Copyright (c) 2010-25 Mike Karlesky, Mark VanderVoord, & Greg Williams
+#   SPDX-License-Identifier: MIT
+# =========================================================================
+
 require 'ceedling/c_extractinator'
 
 class Partializer
@@ -6,14 +13,33 @@ class Partializer
     return !(type.to_s.include?('mock'))
   end
 
-  def extract_public_functions(header_filepath:, source_filepath:)
+  def extract_functions(header_filepath:, source_filepath:, **types)
     extractinator = CExtractinator.from_file(header_filepath)
-    funcs = extractinator.extract_functions()
+    header_funcs = extractinator.extract_functions()
 
     extractinator = CExtractinator.from_file(source_filepath)
-    funcs += extractinator.extract_functions()
+    source_funcs = extractinator.extract_functions()
+    
+    impl = []
+    interface = []
 
-    return funcs
+    types.each do |type|
+      case type
+      when :test_public
+        impl += filter_public_funcs(header_funcs)
+        impl += filter_public_funcs(source_funcs)
+      when :test_private
+        impl += filter_private_funcs(header_funcs)
+        impl += filter_private_funcs(source_funcs)
+      when :mock_public
+        interface += filter_public_funcs(source_funcs)
+      when :mock_private
+        interface += filter_private_funcs(source_funcs)
+      end
+    end
+
+    return impl, interface
   end
+
 
 end
