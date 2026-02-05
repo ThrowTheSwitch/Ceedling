8c66c3cda565ccd51c6a45d4395b08fda6bba11a
Michael Karlesky <michael@karlesky.net>
2026-02-05 14:06:06 -0500

    🎨 Constants, structs, and instantiation
    - Centralized Partials symbols
    - Migrated Partials configuration hashes to `Struct`s
    - Renaming for brevity

--- lib/ceedling/partials.rb
+++ lib/ceedling/partials.rb
@@ -5,7 +5,28 @@
 #   SPDX-License-Identifier: MIT
 # =========================================================================
 
+
 module Partials
+  # Constants
+  TEST_PUBLIC  = :test_public
+  TEST_PRIVATE = :test_private
+  MOCK_PUBLIC  = :mock_public
+  MOCK_PRIVATE = :mock_private
+
+  # Data class representing a source or header file to be partialized
+  ConfigFileInfo = Struct.new(:filepath, :preprocessed_filepath, :includes, keyword_init: true) do
+    def initialize(filepath: nil, preprocessed_filepath: nil, includes: [])
+      super
+    end
+  end
+
+  # Data class representing a C partial to be generated
+  Config = Struct.new(:module, :types, :header, :source, keyword_init: true) do
+    def initialize(module:, types: [], header: ConfigFileInfo.new, source: ConfigFileInfo.new)
+      super
+    end
+  end
+
   # Data class representing a C function signature
   FunctionDeclaration = Struct.new(
     :signature,       # FunctionDefinition signature (e.g., "int foo(void)")
@@ -33,7 +54,11 @@ module Partials
     end
   end
 
-  def self.manufacture_function_declaration_struct(line_num: nil, source_filepath: nil, signature:)
+  def self.manufacture_config(module_name:)
+    return Config.new(module: module_name)
+  end
+
+  def self.manufacture_function_declaration(line_num: nil, source_filepath: nil, signature:)
     return FunctionDeclaration.new(
       signature: signature,
       source_filepath: source_filepath,
@@ -41,7 +66,7 @@ module Partials
     )
   end
 
-  def self.manufacture_function_definition_struct(line_num: nil, source_filepath: nil, signature:, code_block:)
+  def self.manufacture_function_definition(line_num: nil, source_filepath: nil, signature:, code_block:)
     return FunctionDefinition.new(
       signature: signature,
       code_block: code_block,
