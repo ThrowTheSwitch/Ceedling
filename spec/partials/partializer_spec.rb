27b4530441e95a7f2ba0df418f15cd116fe957b8
Michael Karlesky <michael@karlesky.net>
2026-02-13 16:47:05 -0500

    ✨ Reconstruct variable declarations
    Remove private keywords and qype qualifier keywords

--- spec/partials/partializer_spec.rb
+++ spec/partials/partializer_spec.rb
@@ -25,6 +25,10 @@ describe Partializer do
     )
   end
 
+  ###
+  ### assemble_configs()
+  ###
+
   context "#assemble_configs" do
     it "returns empty hash when test_context_configs is empty" do
       test_context_configs = []
@@ -68,6 +72,10 @@ describe Partializer do
       expect(result).to eq(mock_configs)
     end
   end
+
+  ###
+  ### sanitize_includes()
+  ###
 
   context "#sanitize_includes" do
     it "returns empty array when input is empty" do
@@ -130,6 +138,10 @@ describe Partializer do
       expect(result.length).to eq(3)
     end
   end
+
+  ###
+  ### remap_implementation_header_includes()
+  ###
 
   context "#remap_implementation_header_includes" do
     it "returns empty array when input is empty and no partials" do
@@ -299,6 +311,10 @@ describe Partializer do
       expect(result).not_to include('partial2.h')
     end
   end
+
+  ###
+  ### remap_implementation_source_includes()
+  ###
 
   context "#remap_implementation_source_includes" do
     it "returns implementation header when input is empty and no partials" do
@@ -590,6 +606,10 @@ describe Partializer do
     end
   end
 
+  ###
+  ### extract_module_contents()
+  ###
+
   context "#extract_module_contents" do
     before(:each) do
       @mock_extractinator = double('CExtractor')
@@ -604,17 +624,17 @@ describe Partializer do
 
       expect(CExtractor).not_to receive(:from_file).with(nil)
 
-      expect(result.funcs).to eq([])
-      expect(result.vars).to eq([])
+      expect(result.function_definitions).to eq([])
+      expect(result.variables).to eq([])
     end
 
     it "extracts contents from header file only" do
       header_contents = CExtractor::CModule.new(
-        funcs: [
+        function_definitions: [
           double('func1', name: 'func1', signature: 'void func1(void)'),
           double('func2', name: 'func2', signature: 'int func2(int x)')
         ],
-        vars: []
+        variables: []
       )
       
       expect(CExtractor).to receive(:from_file).with('/path/to/header.h').and_return(@mock_extractinator)
@@ -630,11 +650,11 @@ describe Partializer do
 
     it "extracts contents from source file only" do
       source_contents = CExtractor::CModule.new(
-        funcs: [
+        function_definitions: [
           double('func1', name: 'func1', signature: 'static void func1(void)'),
           double('func2', name: 'func2', signature: 'static int func2(int x)')
         ],
-        vars: []
+        variables: []
       )
       
       expect(CExtractor).to receive(:from_file).with('/path/to/source.c').and_return(@mock_extractinator)
@@ -650,18 +670,18 @@ describe Partializer do
 
     it "extracts and combines contents from both header and source files" do
       header_contents = CExtractor::CModule.new(
-        funcs: [
+        function_definitions: [
           double('func1', name: 'func1', signature: 'void func1(void)'),
           double('func2', name: 'func2', signature: 'int func2(int x)')
         ],
-        vars: [double('var1', name: 'header_var')]
+        variables: [double('var1', name: 'header_var')]
       )
       source_contents = CExtractor::CModule.new(
-        funcs: [
+        function_definitions: [
           double('func3', name: 'func3', signature: 'static void func3(void)'),
           double('func4', name: 'func4', signature: 'static int func4(int x)')
         ],
-        vars: [double('var2', name: 'source_var')]
+        variables: [double('var2', name: 'source_var')]
       )
       
       header_extractinator = double('header_extractinator')
@@ -677,12 +697,12 @@ describe Partializer do
         source_filepath: '/path/to/source.c'
       )
       
-      expect(result.funcs).to eq(header_contents.funcs + source_contents.funcs)
-      expect(result.vars).to eq(header_contents.vars + source_contents.vars)
+      expect(result.function_definitions).to eq(header_contents.function_definitions + source_contents.function_definitions)
+      expect(result.variables).to eq(header_contents.variables + source_contents.variables)
     end
 
     it "returns empty CModule when header file has no contents" do
-      empty_contents = CExtractor::CModule.new(funcs: [], vars: [])
+      empty_contents = CExtractor::CModule.new(function_definitions: [], variables: [])
       
       expect(CExtractor).to receive(:from_file).with('/path/to/header.h').and_return(@mock_extractinator)
       expect(@mock_extractinator).to receive(:extract_contents).and_return(empty_contents)
@@ -692,12 +712,12 @@ describe Partializer do
         source_filepath: nil
       )
       
-      expect(result.funcs).to eq([])
-      expect(result.vars).to eq([])
+      expect(result.function_definitions).to eq([])
+      expect(result.variables).to eq([])
     end
 
     it "returns empty CModule when source file has no contents" do
-      empty_contents = CExtractor::CModule.new(funcs: [], vars: [])
+      empty_contents = CExtractor::CModule.new(function_definitions: [], variables: [])
       
       expect(CExtractor).to receive(:from_file).with('/path/to/source.c').and_return(@mock_extractinator)
       expect(@mock_extractinator).to receive(:extract_contents).and_return(empty_contents)
@@ -707,14 +727,18 @@ describe Partializer do
         source_filepath: '/path/to/source.c'
       )
       
-      expect(result.funcs).to eq([])
-      expect(result.vars).to eq([])
+      expect(result.function_definitions).to eq([])
+      expect(result.variables).to eq([])
     end
   end
 
+  ###
+  ### reconstruct_functions()
+  ###
+
   context "#reconstruct_functions" do
     it "returns empty arrays when no functions are provided" do
-      contents = CExtractor::CModule.new(funcs: [], vars: [])
+      contents = CExtractor::CModule.new(function_definitions: [], variables: [])
       types = []
       
       impl, interface = @partializer.reconstruct_functions(
@@ -731,7 +755,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::TEST_PUBLIC]
       
       filtered_impl = [{ name: 'public_func', type: :impl }]
@@ -755,7 +779,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::TEST_PRIVATE]
       
       filtered_impl = [{ name: 'private_func', type: :impl }]
@@ -779,7 +803,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::MOCK_PUBLIC]
       
       filtered_interface = [{ name: 'public_func', type: :interface }]
@@ -803,7 +827,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::MOCK_PRIVATE]
       
       filtered_interface = [{ name: 'private_func', type: :interface }]
@@ -827,7 +851,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::TEST_PUBLIC, Partials::TEST_PRIVATE]
       
       filtered_public_impl = [{ name: 'public_func', type: :impl }]
@@ -857,7 +881,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::MOCK_PUBLIC, Partials::MOCK_PRIVATE]
       
       filtered_public_interface = [{ name: 'public_func', type: :interface }]
@@ -887,7 +911,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [Partials::TEST_PUBLIC, Partials::MOCK_PRIVATE]
       
       filtered_public_impl = [{ name: 'public_func', type: :impl }]
@@ -917,7 +941,7 @@ describe Partializer do
         double('func1', name: 'public_func'),
         double('func2', name: 'private_func')
       ]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = []
       
       impl, interface = @partializer.reconstruct_functions(
@@ -931,7 +955,7 @@ describe Partializer do
 
     it "raises error for invalid partial type" do
       mock_funcs = [double('func1', name: 'public_func')]
-      contents = CExtractor::CModule.new(funcs: mock_funcs, vars: [])
+      contents = CExtractor::CModule.new(function_definitions: mock_funcs, variables: [])
       types = [:invalid_type]
       
       expect {
@@ -940,6 +964,239 @@ describe Partializer do
           types: types
         )
       }.to raise_error(ArgumentError)
+    end
+  end
+
+  ###
+  ### reconstruct_variables()
+  ###
+
+  context "#reconstruct_variables" do
+    it "returns empty array when no variables are provided" do
+      result = @partializer.reconstruct_variables(variables: [])
+      
+      expect(result).to eq([])
+    end
+
+    it "returns variable unchanged when no keywords present" do
+      variables = ['int counter;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int counter;'])
+    end
+
+    it "removes static keyword from variable declaration" do
+      variables = ['static int counter;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int counter;'])
+    end
+
+    it "removes const keyword from variable declaration" do
+      variables = ['const int MAX_VALUE;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int MAX_VALUE;'])
+    end
+
+    it "removes volatile keyword from variable declaration" do
+      variables = ['volatile int status;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int status;'])
+    end
+
+    it "removes multiple keywords from single declaration" do
+      variables = ['static const int MAX_VALUE;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int MAX_VALUE;'])
+    end
+
+    it "removes all type qualifiers from declaration" do
+      variables = ['static const volatile int flags;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int flags;'])
+    end
+
+    it "preserves variable names containing keyword substrings" do
+      variables = ['int my_static_var;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int my_static_var;'])
+    end
+
+    it "handles pointer declarations" do
+      variables = ['static int* ptr;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int* ptr;'])
+    end
+
+    it "handles array declarations" do
+      variables = ['static int array[10];']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int array[10];'])
+    end
+
+    it "handles complex pointer declarations" do
+      variables = ['const char* const ptr;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['char* ptr;'])
+    end
+
+    it "normalizes multiple spaces to single space" do
+      variables = ['static  const   int    value;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int value;'])
+    end
+
+    it "removes leading and trailing whitespace" do
+      variables = ['  static int counter;  ']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int counter;'])
+    end
+
+    it "processes multiple variable declarations" do
+      variables = [
+        'static int counter;',
+        'const float pi;',
+        'volatile bool flag;'
+      ]
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq([
+        'int counter;',
+        'float pi;',
+        'bool flag;'
+      ])
+    end
+
+    it "handles struct declarations with keywords" do
+      variables = ['static struct Point position;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['struct Point position;'])
+    end
+
+    it "handles typedef declarations with keywords" do
+      variables = ['static MyType_t instance;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['MyType_t instance;'])
+    end
+
+    it "handles unsigned types with keywords" do
+      variables = ['static unsigned int count;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['unsigned int count;'])
+    end
+
+    it "handles long types with keywords" do
+      variables = ['static long long int big_number;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['long long int big_number;'])
+    end
+
+    it "handles function pointer declarations" do
+      variables = ['static void (*callback)(int);']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['void (*callback)(int);'])
+    end
+
+    it "preserves initialization values" do
+      variables = ['static int counter = 0;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int counter = 0;'])
+    end
+
+    it "handles complex initialization with keywords" do
+      variables = ['static const int values[] = {1, 2, 3};']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int values[] = {1, 2, 3};'])
+    end
+
+    it "handles keywords in middle of declaration" do
+      variables = ['int static counter;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int counter;'])
+    end
+
+    it "handles multiple const keywords" do
+      variables = ['const int* const ptr;']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq(['int* ptr;'])
+    end
+
+    it "does not remove keywords from string literals" do
+      variables = ['char* str = "static const";']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      # Note: This test documents current behavior - the method doesn't 
+      # handle string literals specially, but since we're removing whole
+      # words with word boundaries, it shouldn't match inside strings
+      expect(result).to eq(['char* str = "static const";'])
+    end
+
+    it "handles empty string in variables array" do
+      variables = ['']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq([''])
+    end
+
+    it "handles whitespace-only string in variables array" do
+      variables = ['   ']
+      
+      result = @partializer.reconstruct_variables(variables: variables)
+      
+      expect(result).to eq([''])
+    end
+
+    it "preserves original array and returns new array" do
+      original_variables = ['static int counter;']
+      
+      result = @partializer.reconstruct_variables(variables: original_variables)
+      
+      expect(original_variables).to eq(['static int counter;'])
+      expect(result).to eq(['int counter;'])
+      expect(result).not_to equal(original_variables)
     end
   end
 
