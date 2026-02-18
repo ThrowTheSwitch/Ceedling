4e9bb68753254854d990f465b56b029b86468a73
Michael Karlesky <michael@karlesky.net>
2026-02-18 16:42:51 -0500

    ✨ Source file collection minus test handling
    New function that does not rely on unnecessary test-specific preprocessing steps

--- lib/ceedling/preprocessinator_file_assembler.rb
+++ lib/ceedling/preprocessinator_file_assembler.rb
@@ -213,8 +213,32 @@ class PreprocessinatorFileAssembler
     return contents, test_directives
   end
 
+  def collect_source_file_contents(source_filepath:, test:, flags:, defines:, include_paths:)
+    contents = []
 
-  def assemble_preprocessed_source_file(filename:, preprocessed_filepath:, contents:, extras:, includes:)
+    preprocessed_filepath = @file_path_utils.form_preprocessed_file_full_expansion_filepath( source_filepath, test )
+
+    # Run GCC with full preprocessor expansion
+    command = @tool_executor.build_command_line(
+      @configurator.tools_test_file_full_preprocessor,
+      # Additional arguments
+      flags,
+      # Argument replacement
+      source_filepath,
+      preprocessed_filepath,
+      defines,
+      include_paths
+    )
+    @tool_executor.exec( command )
+
+    @file_wrapper.open( preprocessed_filepath, 'r' ) do |file|
+      contents = @preprocessinator_extractor.extract_file_as_array_from_expansion( file, source_filepath )
+    end
+
+    return contents
+  end
+
+  def assemble_preprocessed_code_file(filename:, preprocessed_filepath:, contents:, extras:, includes:)
     _contents = []
 
     # Reinsert #include statements into stripped down file
