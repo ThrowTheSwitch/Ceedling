4f52f5eea48a5907500bf92f5f47a7e4c12a3c6c
Michael Karlesky <michael@karlesky.net>
2026-04-19 14:18:15 -0400

    ✨ PARTIAL_TEST_ALL_MODULE, PARTIAL_MOCK_ALL_MODULE

--- lib/ceedling/ceedling.h
+++ lib/ceedling/ceedling.h
@@ -16,10 +16,12 @@
 // Partials directive macros encoding gross configuration
 #define TEST_PARTIAL_PUBLIC_MODULE(module) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CEEDLING_PARTIALS_PREFIX)__PARTIALS_EXPAND(module)__PARTIALS_EXPAND(_impl.h))
 #define TEST_PARTIAL_PRIVATE_MODULE(module) TEST_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
+#define TEST_PARTIAL_ALL_MODULE(module) TEST_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
 #define MOCK_PARTIAL_PUBLIC_MODULE(module) __PARTIALS_STRINGIFY(__PARTIALS_EXPAND(CMOCK_MOCK_PREFIX)__PARTIALS_EXPAND(CEEDLING_PARTIALS_PREFIX)__PARTIALS_EXPAND(module)__PARTIALS_EXPAND(_interface.h))
 #define MOCK_PARTIAL_PRIVATE_MODULE(module) MOCK_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
+#define MOCK_PARTIAL_ALL_MODULE(module) MOCK_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
 
-// Partials directive macros needing configuration
+// Partials directive macros requiring configuration
 #define TEST_PARTIAL_MODULE(module) TEST_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
 #define MOCK_PARTIAL_MODULE(module) MOCK_PARTIAL_PUBLIC_MODULE(module) // Deduplicate macro definition
 
